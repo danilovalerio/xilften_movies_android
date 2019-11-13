b275874728be0566e9a59621eb8ef75e471ed821
dvalerio <dnotms290>
2019-11-13 13:34:05 -0300

    Models movies e categorys

--- app/src/main/java/com/danilovalerio/xilftenmovies/model/Movie.java
+++ app/src/main/java/com/danilovalerio/xilftenmovies/model/Movie.java
@@ -2,6 +2,13 @@ package com.danilovalerio.xilftenmovies.model;
 
 public class Movie {
 
+    private int coverUrl;
 
+    public int getCoverUrl() {
+        return coverUrl;
+    }
 
+    public void setCoverUrl(int coverUrl) {
+        this.coverUrl = coverUrl;
+    }
 }
