57f401b3f1835d3871d4399475d9051211b1e5da
Danilo <danilovaleriosilva@gmail.com>
2019-11-17 16:39:04 -0300

    Sobrepondo o play na imagem

--- app/src/main/java/com/danilovalerio/xilftenmovies/MovieActivity.java
+++ app/src/main/java/com/danilovalerio/xilftenmovies/MovieActivity.java
@@ -2,8 +2,13 @@ package com.danilovalerio.xilftenmovies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
 public class MovieActivity extends AppCompatActivity {
 
@@ -19,6 +24,18 @@ public class MovieActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back_white_24dp);
         }
+
+        //Pegar o drawable, e para manter compatibilidade com dispositivos antigos usamos o ContextCompat
+        LayerDrawable drawable = (LayerDrawable) ContextCompat.getDrawable(this, R.drawable.shadows);
+
+        //Realiza a troca da imagem que está lá no shadows
+        if (drawable != null){
+            Drawable movieCover = ContextCompat.getDrawable(this, R.drawable.movie);
+//            drawable.setDrawableByLayerId(R.id.cover_drawable, movieCover);
+//            ((ImageView) findViewById(R.id.image_view_cover)).setImageDrawable(drawable);
+        }
+
+
     }
 
 }
