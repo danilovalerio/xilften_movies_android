576ee5017731644046ae142c22200577513af10e
Danilo <danilovaleriosilva@gmail.com>
2019-11-17 15:42:03 -0300

    Configurações Toolbar MovieActivity

--- app/src/main/java/com/danilovalerio/xilftenmovies/MovieActivity.java
+++ app/src/main/java/com/danilovalerio/xilftenmovies/MovieActivity.java
@@ -3,6 +3,7 @@ package com.danilovalerio.xilftenmovies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 
 public class MovieActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class MovieActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //configuraões da toolbar
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back_white_24dp);
+        }
     }
+
 }
