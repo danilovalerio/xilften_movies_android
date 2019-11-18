65aff7e315a2185c384f7cadff735bd3396979ba
Danilo <danilovaleriosilva@gmail.com>
2019-11-18 11:20:36 -0300

    Escopo da AsyncTask

--- app/src/main/java/com/danilovalerio/xilftenmovies/util/JsonDownloadTask.java
+++ app/src/main/java/com/danilovalerio/xilftenmovies/util/JsonDownloadTask.java
@@ -1,6 +1,33 @@
 package com.danilovalerio.xilftenmovies.util;
 
-public class JsonDownloadTask {
+import android.os.AsyncTask;
 
+import com.danilovalerio.xilftenmovies.model.Category;
 
+import java.util.List;
+
+/*AsyncTask
+Espera 3 propriedades
+- sequência de parâmetros
+- espera progress se quiser
+- resultado da operação
+ */
+public class JsonDownloadTask extends AsyncTask<String, Void, List<Category>> {
+    //main-thread (exibir uma progressbar)
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    // thread - background
+    @Override
+    protected List<Category> doInBackground(String... params) {
+        return null;
+    }
+
+    //main-thread (ocultar uma progressbar)
+    @Override
+    protected void onPostExecute(List<Category> categories) {
+        super.onPostExecute(categories);
+    }
 }
